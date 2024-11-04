b4ceb9d02732d7f6e734bcf2a38060567b01b5b0
Doan Quoc Kiet <116879984+doanquockiet@users.noreply.github.com>
2024-11-04 20:39:02 +0700

    updateupdate:

--- src/components/CoffeeAndTea/ProductCoffee.js
+++ src/components/CoffeeAndTea/ProductCoffee.js
@@ -3,23 +3,23 @@ import { Coffees } from '../../data/coffee'
 import { Card, Col, Row } from 'react-bootstrap'
 
 
-export default function ProductCoffee() { 
-   
-        return (
-          <div className='container'>
-          <Row className="row-cols-1 row-cols-md-2 row-cols-lg-6 container-j">
-            {Coffees.map((newpage) => (
-              <Col key={newpage.id} className="news-card">
-                <Card style={{height:'400px', border:'none'}} onClick ={() => navigate(`/tea/${newpage?.id}`)}>
-                  <Card.Img variant="top" style={{ boxShadow: '0 4px 8px rgba(0, 0, 0, 0.2)',height: '300px',objectFit: 'cover', height:'300px', borderRadius:'5px'}} src={newpage.image} />
-                  <Card.Body >
-                    <Card.Title style={{fontSize:'18px', textAlign:'start'}}>{newpage.title}</Card.Title>
-                    <Card.Text style={{textAlign:"start"}}>{newpage.price}</Card.Text>
-                   </Card.Body>
-                  </Card>
-              </Col>
-            ))}
-          </Row>
-        </div>
-        )
-      }
+export default function ProductCoffee() {
+
+    return (
+        <div className='container'>
+            <Row className="row-cols-1 row-cols-md-2 row-cols-lg-6 container-j">
+                {Coffees.map((newpage) => (
+                    <Col key={newpage.id} className="news-card">
+                        <Card style={{ height: '400px', border: 'none' }} >
+                            <Card.Img variant="top" style={{ boxShadow: '0 4px 8px rgba(0, 0, 0, 0.2)', height: '300px', objectFit: 'cover', height: '300px', borderRadius: '5px' }} src={newpage.image} />
+                            <Card.Body >
+                                <Card.Title style={{ fontSize: '18px', textAlign: 'start' }}>{newpage.title}</Card.Title>
+                                <Card.Text style={{ textAlign: "start" }}>{newpage.price}</Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                ))}
+            </Row>
+        </div>
+    )
+}
