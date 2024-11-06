3bed03ae1784fdfe9616abdecd08b8231e4656b5
nguyenkun112 <nguyendung070203@gmail.com>
2024-11-06 17:18:35 +0700

    DetailTeaAndCoffee

--- src/components/CoffeeAndTea/ProductCoffee.jsx
+++ src/components/CoffeeAndTea/ProductCoffee.jsx
@@ -1,37 +1,27 @@
-import React from "react";
-import { Coffees } from "../../data/coffee";
-import { Card, Col, Row } from "react-bootstrap";
+import React from 'react'
+import { Coffees } from '../../data/coffee'
+import { Card, Col, Row } from 'react-bootstrap'
 
 export default function ProductCoffee() {
   return (
-    <div className="container">
+    <div>
+      <div>
       <Row className="row-cols-1 row-cols-md-2 row-cols-lg-6 container-j">
         {Coffees.map((newpage) => (
-          <Col key={newpage.id} className="news-card">
-            <Card style={{ height: "400px", border: "none" }}>
-              <Card.Img
-                variant="top"
-                style={{
-                  boxShadow: "0 4px 8px rgba(0, 0, 0, 0.2)",
-                  height: "300px",
-                  objectFit: "cover",
-
-                  borderRadius: "5px",
-                }}
-                src={newpage.image}
-              />
+          <Col key={newpage.id} className="news-card mb-4">
+            <Card style={{height:'300px', border:'none'}}>
+              <Card.Img variant="top" src={newpage.image} />
               <Card.Body>
-                <Card.Title style={{ fontSize: "18px", textAlign: "start" }}>
-                  {newpage.title}
-                </Card.Title>
-                <Card.Text style={{ textAlign: "start" }}>
-                  {newpage.price}
-                </Card.Text>
-              </Card.Body>
-            </Card>
+                <Card.Title>{newpage.title}</Card.Title>
+                <Card.Text>{newpage.price}</Card.Text>
+                </Card.Body>
+               <div className='' style={{marginLeft:'10px', marginRight:'10px' , marginBottom:'10px', justifyContent:'space-between', display:'flex'}}>
+               </div>
+              </Card>
           </Col>
         ))}
       </Row>
     </div>
-  );
+    </div>
+  )
 }
